06ad2b726ca92f720e3560fbe6acc779c0a168be
John Genere <johncgenere@gmail.com>
2019-05-30 13:29:46 -0400

    Created function in js file to check password

--- form/form.js
+++ form/form.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+function checkPassword(){
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  let email = document.getElementById('email').value;
+  
+  if(password !== '12345678' || username === "" || email === ""){
+    window.alert('Incorrect Information:\nCould be empty username/email or wrong password.\nTRY AGAIN!');
+    document.getElementById('text').textContent = "Is Your Info Right?";
+  } else {
+    document.getElementById('text').textContent = "Correct!";
+  }
+}
